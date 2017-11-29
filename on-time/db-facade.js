b394260027569b49ee2d34a2d4d3ed669a9de193
joachimprinzbach <joachim.prinzbach@gmail.com>
2017-11-29 22:49:17 +0100

    Minor refactoring

--- on-time/db-facade.js
+++ on-time/db-facade.js
@@ -1,9 +1,36 @@
 const searchForConnections = require('./search-page.facade');
 const getDelayMessages = require('./result-page.facade');
+const dbSearchPageURL = 'https://www.bahn.de/p/view/index.shtml';
+const moment = require('moment-timezone');
+moment.locale('de');
+moment.tz.setDefault("Europe/Berlin");
 
-const getDelays = async (page, startStation, targetStation, exactDepartureTime, minDelay) => {
-        await searchForConnections(page, startStation, targetStation, exactDepartureTime);
-        return getDelayMessages(page, exactDepartureTime, minDelay);
+const getDelayMessagesFor = async (connection) => {
+    const {browser, page} = await openBrowserWindow(dbSearchPageURL);
+
+    const startStation = connection.start;
+    const targetStation = connection.destination;
+    const exactDepartureTime = moment(connection.exactConnection).format('HH:mm');
+    const minDelay = connection.minDelay;
+
+    await searchForConnections(page, startStation, targetStation, exactDepartureTime);
+    const delayMessages = getDelayMessages(page, exactDepartureTime, minDelay);
+    browser.close();
+    return delayMessages;
+
 };
 
-module.exports = getDelays;
+const openBrowserWindow = async urlToOpen => {
+    const browser = await puppeteer.launch({
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+        ],
+    });
+    const page = await browser.newPage();
+    page.setViewport({width: 1200, height: 900});
+    await page.goto(urlToOpen, {waitUntil: 'networkidle2'});
+    return {browser, page};
+};
+
+module.exports = getDelayMessagesFor;

3da5992f5f6851688e17346a9aa0d5711d52e765
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2017-12-03 21:38:27 +0100

    add describe for test method

--- on-time/crawl.spec.js
+++ on-time/crawl.spec.js
@@ -3,69 +3,93 @@ const moment = require('moment');
 const assert = require('assert');
 
 describe('crawl', () => {
-    it('should determine timeframe correctly', () => {
-        let MONDAY_6_32 = 1512365544000;
-        moment.now = function () {
-            return MONDAY_6_32;
-        };
-        const connection = {
-            runOnWeekend: false,
-            crawlStart: {
-                "hour": "6",
-                "minute": "1"
-            },
-            crawlEnd: {
-                "hour": "6",
-                "minute": "40"
-            },
-        };
+    describe('shouldCheckForDelaysOf', () => {
+        it('should determine timeframe correctly', () => {
+            let MONDAY_6_32 = 1512365544000;
+            moment.now = function () {
+                return MONDAY_6_32;
+            };
+            const connection = {
+                runOnWeekend: false,
+                crawlStart: {
+                    "hour": "6",
+                    "minute": "1"
+                },
+                crawlEnd: {
+                    "hour": "6",
+                    "minute": "40"
+                }
+            };
 
-        const shouldCheck = crawl.shouldCheckForDelaysOf(connection);
+            const shouldCheck = crawl.shouldCheckForDelaysOf(connection);
 
-        assert.equal(true, shouldCheck);
-    });
+            assert.equal(true, shouldCheck);
+        });
 
-    it('should be possible to check weekends', () => {
-        let SUDNAY_6_32 = 1512279144000;
-        moment.now = function () {
-            return SUDNAY_6_32;
-        };
-        const connection = {
-            runOnWeekend: true,
-            crawlStart: {
-                "hour": "6",
-                "minute": "1"
-            },
-            crawlEnd: {
-                "hour": "6",
-                "minute": "40"
-            },
-        };
+        it('should not run outside of timeframe', () => {
+            let MONDAY_6_32 = 1512365544000;
+            moment.now = function () {
+                return MONDAY_6_32;
+            };
+            const connection = {
+                runOnWeekend: false,
+                crawlStart: {
+                    "hour": "6",
+                    "minute": "40"
+                },
+                crawlEnd: {
+                    "hour": "7",
+                    "minute": "40"
+                }
+            };
 
-        const shouldCheck = crawl.shouldCheckForDelaysOf(connection);
+            const shouldCheck = crawl.shouldCheckForDelaysOf(connection);
 
-        assert.equal(true, shouldCheck);
-    });
+            assert.equal(false, shouldCheck);
+        });
 
-    it('should not run on weekends weekends', () => {
-        let SUDNAY_6_32 = 1512279144000;
-        moment.now = function () {
-            return SUDNAY_6_32;
-        };
-        const connection = {
-            runOnWeekend: false,
-            crawlStart: {
-                "hour": "6",
-                "minute": "1"
-            },
-            crawlEnd: {
-                "hour": "6",
-                "minute": "40"
-            },
-        };
+        it('should be possible to check weekends', () => {
+            let SUDNAY_6_32 = 1512279144000;
+            moment.now = function () {
+                return SUDNAY_6_32;
+            };
+            const connection = {
+                runOnWeekend: true,
+                crawlStart: {
+                    "hour": "6",
+                    "minute": "1"
+                },
+                crawlEnd: {
+                    "hour": "6",
+                    "minute": "40"
+                }
+            };
 
-        const shouldCheck = crawl.shouldCheckForDelaysOf(connection);
+            const shouldCheck = crawl.shouldCheckForDelaysOf(connection);
 
-        assert.equal(false, shouldCheck);
+            assert.equal(true, shouldCheck);
+        });
+
+        it('should not run on weekends weekends', () => {
+            let SUDNAY_6_32 = 1512279144000;
+            moment.now = function () {
+                return SUDNAY_6_32;
+            };
+            const connection = {
+                runOnWeekend: false,
+                crawlStart: {
+                    "hour": "6",
+                    "minute": "1"
+                },
+                crawlEnd: {
+                    "hour": "6",
+                    "minute": "40"
+                }
+            };
+
+            const shouldCheck = crawl.shouldCheckForDelaysOf(connection);
+
+            assert.equal(false, shouldCheck);
+        });
     });
 });
